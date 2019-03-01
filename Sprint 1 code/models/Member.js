4b2248b8ff8538739038f35159a8c03b469753c4
Mariam Heikal <mariamheikal228@gmail.com>
2019-03-01 21:20:05 +0200

    Models

---
+++ Sprint 1 code/models/Member.js
@@ -0,0 +1,9 @@
+
+const uuid = require('uuid')
+
+module.exports= Member=[
+    {name:'Eman' , ID:1,field:'ENG', activation:false,MemberTasks:['task1','task2']},
+    
+    {name:'Janna' , ID:2, field:'PHARM' ,MemberTasks:[]},
+    {name:'Lina' , ID:3,field:'PHARM', MemberTasks:[]}
+];
