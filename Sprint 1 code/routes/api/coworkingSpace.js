f7e075202a03af327c7062308f1292202c4f4466
nourhan <nourhan.ahmedkhalifa@student.guc.edu.eg>
2019-03-02 18:56:25 +0200

    Update coworkingSpace.js

--- Sprint 1 code/routes/api/coworkingSpace.js
+++ Sprint 1 code/routes/api/coworkingSpace.js
@@ -1,8 +1,1 @@
-const express = require('express');
-const router = express.Router();
 
-const PartnerCoworkingSpace = require('../../models/PartnerCoworkingSpace');
-
-router.get('/', (req, res) => res.json({ data: PartnerCoworkingSpace }));
-
-module.exports = router;
