9d00e35d5c2d91d43edc1d39a13360df9853d941
nourhan <nourhan.ahmedkhalifa@student.guc.edu.eg>
2019-03-02 18:55:43 +0200

    Update RoomBookingsAPI.js

--- Sprint 1 code/routes/api/RoomBookingsAPI.js
+++ Sprint 1 code/routes/api/RoomBookingsAPI.js
@@ -1,8 +1,1 @@
-const express = require('express');
-const router = express.Router();
 
-const RoomBookingsAPI = require('../../models/RoomBookings');
-
-router.get('/', (req, res) => res.json({ data: RoomBookings }));
-
-module.exports = router;
