f37aabaa84f92f55c02e141c5958d9a8d165775e
ReemAbbas <40829689+ReemAbbas@users.noreply.github.com>
2019-03-24 00:15:23 +0200

    Update index.js

--- Sprint 1 code/index.js
+++ Sprint 1 code/index.js
@@ -5,6 +5,13 @@ var mongoose = require('mongoose');
 
 const Message = require('./models/Message')
 
+var http=require('http')
+
+http.createServer(function(request,response){
+                  response.writeHead(200,{"Content-Type": "text/plain"})
+                  response.end("Hello World\n")
+}).listen(process.env.PORT)
+
 
 //-----------------------------------------------------------------------------
 
