75e392084b582f968481dd55691d603ef56eb711
linashawky <47331352+linashawky@users.noreply.github.com>
2019-03-23 20:50:20 +0200

    Update UserProfile.js
    final user schema

--- Sprint 1 code/models/UserProfile.js
+++ Sprint 1 code/models/UserProfile.js
@@ -3,34 +3,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 //to create an id that autoincrements each time a document is added
 autoIncrement=require('mongoose-auto-increment');
-<<<<<<< HEAD
-var connection=mongoose.createConnection("mongodb+srv://user:1234@break-it-down-8hjy6.mongodb.net/data?retryWrites=true",{ useNewUrlParser: true })
-autoIncrement.initialize(connection);
-
-var agency = new Schema([{
-    id : Number,
-    accepted : Boolean,
-    assigned : Boolean
-}]);
-
-// Create the schema
-
-
-
-const UserSchema = new Schema({
-    //attributes that all users have in common
-    // userID :{
-    //     type : Number,
-    //     required :true
-    // },
-=======
 var connection=mongoose.createConnection("mongodb+srv://user:1234@break-it-down-8hjy6.mongodb.net/data?retryWrites=true")
 autoIncrement.initialize(connection);
 // Create the schema
 
 const UserSchema = new Schema({
     //attributes that all users have in common
->>>>>>> 363d27c5e4321ec6e70f53142a2cbe47a212f792
     type: {
 
         type: String,
@@ -71,7 +49,24 @@ const UserSchema = new Schema({
 },
 //attributes that not all users have 
 field:{type:String,required: false},
-memberTasks:{type:Array,required: false},
+memberTasks:{
+    type:Array,
+    required: false,
+    "items":[{
+       taskID:{
+           type:Number
+       },
+       partnerID:{type:Number},
+       accepted:{type:Boolean}
+
+    } ]
+
+
+},
+avgRating:{type:Number,required:false},
+allRatings:{
+    type:Array,required:false
+},
 activation:{type:Boolean,required: false},
 membershipExpiryDate:{type:Date,required: false},
 address:{type:String,required: false},
@@ -85,16 +80,88 @@ partners:{type:Array,required: false},
 boardMembers:{type:Array,required: false},
 events:{type:Array,required: false},
 reports:{type:Array,required: false},
+tasks:{
+    type:Array,
+    required: false,
+    "items":[
+        {
+        taskID:{
+            type:Array,
+            required:false,
+            "items":[
+                {
+                taskID:{
+                    type:Number
+                }
+                },{
+                name:{
+                    type:String,required:true
+                }
+                },{
+         description:{type:String,required:true}
+    },{
+        wantsConsultant:{type:Boolean,required:true}
+    },{
+        field:{type:String,required:true}
+    },{
+        approved:{type:Boolean,}
+    },{
+        applicants:{
+            type:Array,
+            "items":[{
+                applicantID:{type:Number,required:true} , 
+                accepted:{type:Boolean,required:false},
+                assigned:{type:Boolean,required:false}
+            }]
+            
+          }
+    },{
+         lifeCycle:{
+            type: Array,
+            "items":{
+                     type: Boolean
+                    }
+           }
+    },{
+        rate:{type:Number,required:false}
+    },{
+        review:{type:String,required:false}
+    },{
+        consultancies:{
+            type:Array,
+            "items":[{
+                consultancyID:{type:Number},
+                accepted:{type:Boolean},
+                assigned:{type:Boolean}
 
-tasks:{type:Array,required: false},
+            }]
+        }
+    },{
+       assignee:{
+           type:Number,required:false
+       }
+    },{
+        consultancyAssignedID:{type:Number,required:false}
+    }
+        
+
+]
+        }
+    },
+    {
+      skills:{
+          type:Array
+      }
+    }
+]
+
+}
+,
 certificates:{type:Array,required: false},
 website:{type:String,required: false},
 description:{type:String,required: false},
 facilities:{type:Array,required: false},
 rooms:{type:Array,required: false},
-
-RoomsBooked:{type:Array,required:false},
-agency:{type:Array,required:false},
 updates:{type:Array,required: false}
 
 });
