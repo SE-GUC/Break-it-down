baf91ac299516ff0e1d69d17bc22583d29e88c15
EmannEssam <47208165+EmannEssam@users.noreply.github.com>
2019-03-31 00:20:11 +0200

    Update partner.test.js

--- Sprint 1 code/partner.test.js
+++ Sprint 1 code/partner.test.js
@@ -1,1 +1,27 @@
 
+const funcs = require('./fn');
+//------------partner getter test------------------//
+test('Partner view applicants for a task', async () => {
+
+  expect.assertions(1)
+  const response =  await funcs.getPartnerTasks();
+  //console.log(response.data.task)
+  expect(typeof response.data.task).toBe('object')
+
+});
+//------------partner put test------------------//
+test('partner review and rate', async () => {
+  expect.assertions(1)
+  const response =  await funcs.partnerReviewRate()
+  if(typeof response.data==="string"){
+    expect(response.data).toContain("the")
+  }
+ 
+  else{
+    expect(response.data.tasks.rate).not.toBe(0)
+    expect(response.data.tasks[0].review).toBeTruthy()
+  }
+  
+
+});
+
