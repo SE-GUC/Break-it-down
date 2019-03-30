514d3def1532654668522c515e848cfa5c7d929a
EmannEssam <47208165+EmannEssam@users.noreply.github.com>
2019-03-31 00:31:40 +0200

    Update fn.js

--- Sprint 1 code/fn.js
+++ Sprint 1 code/fn.js
@@ -1,6 +1,36 @@
 const axios = require('axios');
 
 const functions = {
+         getApprovedTasks: async () => {
+            const allTasks = await axios.get('http://localhost:4000/api/member/allTasks')
+            return allTasks
+            },
+
+    getRecomendedTasks: async () => {
+          //get reco tasks  for member with id 5
+           const recoTasks = await axios.get('http://localhost:4000/api/member/recoTasks/5')
+           return recoTasks
+                },  
+     getMemTasks: async () => {
+           //get membertasks for member with id 5
+            const memTasks = await axios.get('http://localhost:4000/api/member/view/5')
+             return memTasks
+             },
+     getMemAvgRating: async () => {
+          //get membertasks for member with id 5
+           const memRate = await axios.get('http://localhost:4000/api/member/MyRating/5')
+           return memRate
+                    },
+      getPartnerTasks: async () => {
+             
+             const partnertasks = await axios.get('http://localhost:4000/api/partner/view/1/0')
+             return partnertasks
+                    },
+        getPartnerLifeCycle: async () => {
+               
+                 const lifeCycle = await axios.get('http://localhost:4000/api/partner/TaskLifeCycle/1/0')
+                 return lifeCycle
+                                                },
 
 AssignApplicant: async () => {
          const aapplicant = await axios.put('http://localhost:4000/api/admin/AssignMember/1/1')  
