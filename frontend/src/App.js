16dca73f47dd8cf8390225f3e2c33773fcd0a5fc
MalakRassem <47498774+MalakRassem@users.noreply.github.com>
2019-04-07 22:09:47 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,26 @@
-/*<MDBNavItem active>
-              <MDBNavLink to={`./viewCoworkingSpace`}>Home</MDBNavLink>
-            </MDBNavItem>*/
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import {Provider} from 'react-redux'
+import store from './store'
 
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
-import UserHomePage from "./pages/UserHomePage";
-import viewcoWorkingSpace from "./pages/viewCoworkingspace";
-import specificCoworkingSpace from "./pages/specificCoworkingSpace";
-import specificRoom from "./pages/specificRoom";
 class App extends Component {
   render() {
-    const App = () => (
-      <div className="App">
-        <Router>
-          <Route exact path="/" component={UserHomePage} />
-          <Route
-            exact
-            path="/viewCoworkingspace"
-            component={viewcoWorkingSpace}
-          />
-          <Route
-            exact
-            path="/specificCoworkingSpace/:id"
-            component={specificCoworkingSpace}
-          />
-          <Route
-            exact
-            path="/specificRoom/:Coid/:Roid"
-            component={specificRoom}
-          />
-        </Router>
-      </div>
-    );
+
     return (
-      <Router>
-        <App />
-      </Router>
+      <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+
+            <h1>Welcome to LirtenHub :)</h1>
+            <p>Please <a>Login</a> <br></br>
+            or 
+            <br></br>
+            <a>Register</a>
+          </p>
+        </header>
+      </div>
+      </Provider>
     );
   }
 }
