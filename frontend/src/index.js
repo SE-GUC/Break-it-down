bd06b4dcaf86be5cba73230429204b0f619a2982
MalakRassem <malakrassem@outlook.com>
2019-04-13 19:14:53 +0200

    Previous Sprint
    Malak and Nour's files.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,27 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
+import {
+    Route,
+    BrowserRouter,
+    
+  } from "react-router-dom";
+  import {Provider} from 'react-redux'
+  import EventList from './components/EventList'
+  import TaskList from './components/TaskList'
+  import store from './store'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+ReactDOM.render(    <Provider store={store}>
+    <BrowserRouter>
+    <Route exact path="/Tasks" component={TaskList}/>
+    <Route exact path="/Events" component={EventList}/>
+
+
+    </BrowserRouter>
+    <App/>
+
+</Provider>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
