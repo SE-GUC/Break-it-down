585eddfae3b6ca7621a8ecda3c304784b38b3f4b
EmannEssam <47208165+EmannEssam@users.noreply.github.com>
2019-04-14 22:42:01 +0200

    Create partnerform_review.js

---
+++ client/src/App/pages/partnerform_review.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PartnerNavbar from "../../App/components/PartnerNavbar"
+import PartnerSidnav from "../../App/components/PartnerSidenav"
+
+
+const ReviewForm = (props) => {
+
+
+
+    return (
+      
+      <form onSubmit={props.getx}>
+ 
+ <PartnerNavbar/>
+ <PartnerSidnav/>
+         
+          <h1 style={{ color: 'green' }}>
+            Review Your assigned applicant
+          </h1>
+          <input style={{ margin:"50px auto", display:"block" }} type="number" name="rating" placeholder="rating"/>
+          <input style={{ margin:"50px auto", display:"block" }} type="text" name="review" placeholder="review"/>
+
+  
+          <button className="btn btn-warning">Submit review</button>
+
+          <div class="alert alert-secondary" role="alert" style={{ position:"fixed",bottom:0,left:0,right:0}}>
+          Copyright © 2019 Lirten Inc. All Rights Reserved.
+          </div>
+   
+           </form>
+    
+    );
+
+  
+  }
+  
+  
+  
+  
+  export default ReviewForm;
