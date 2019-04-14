f433ec9cf2665ce976926ae002a71fc9cc053a95
ReemAbbas <reem.abbas@student.guc.edu.eg>
2019-04-14 17:38:28 +0200

    Deploying on Heroku

--- server.js
+++ server.js
@@ -39,20 +39,28 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.log(err));
 
-//Static file declaration
-app.use(express.static(path.join(__dirname, 'client/build')));
-//production mode
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  //
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'client/build/index.html'));
-  })
-}
+// //Static file declaration
+// app.use(express.static(path.join(__dirname, 'client/build')));
+// //production mode
+// if(process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+  
+//   app.get('*', (req, res) => {
+//     res.sendfile(path.join(__dirname = 'client/build/index.html'));
+//   })
+// }
 //build mode
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/public/index.html'));
 })
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(client.build));
+
+  app.get('*',(req,res) =>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
+
+  })
+}
 
 //--------------------Init middleware--------------------
 app.use(express.json());
