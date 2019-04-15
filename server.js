01eb20af48c4b58666276661da6e93107212ab24
shazaelmorsh <shaza_1998@hotmail.com>
2019-04-15 03:40:56 +0200

    final

--- server.js
+++ server.js
@@ -51,16 +51,6 @@ mongoose
 //   })
 // }
 //build mode
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/public/index.html"));
-});
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(client.build));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 //--------------------Init middleware--------------------
 app.use(express.json());
@@ -102,7 +92,16 @@ app.use("/api/partner", partner);
 app.use("/api/consultancyAgency", ca);
 app.use("/api/CreateAccount", ProfilesAPI);
 app.use("/api/posts", posts);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(client.build));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 //--------------------Handling Error 404--------------------
 app.use((req, res) => {
   res.status(404).send({ err: "We can not find what you are looking for" });
