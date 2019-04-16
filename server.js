8b6e62a52fa79dfc027f8f80c77aa6f0c2838850
ReemAbbas <reem.abbas@student.guc.edu.eg>
2019-04-16 02:35:54 +0200

    prepare to deploy

--- server.js
+++ server.js
@@ -50,6 +50,13 @@ mongoose
 //     res.sendfile(path.join(__dirname = 'client/build/index.html'));
 //   })
 // }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 //build mode
 
 //--------------------Init middleware--------------------
@@ -95,13 +102,7 @@ app.use("/api/posts", posts);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/public/index.html"));
 });
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 //--------------------Handling Error 404--------------------
 app.use((req, res) => {
   res.status(404).send({ err: "We can not find what you are looking for" });
