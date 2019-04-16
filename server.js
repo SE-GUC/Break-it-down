6b977bc15c3b07379525b3e59e8452f24ccb9616
ReemAbbas <reem.abbas@student.guc.edu.eg>
2019-04-16 02:27:46 +0200

    prepare to deploy

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/public/index.html"));
 });
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(client.build));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
