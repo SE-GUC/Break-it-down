a18bb13fd685e4033363dd06ae96ee409ed67e15
EmannEssam <47208165+EmannEssam@users.noreply.github.com>
2019-04-23 02:25:15 +0200

    Update partnerform.js

--- client/src/pages/partnerform.js
+++ client/src/pages/partnerform.js
@@ -7,31 +7,41 @@ const UserForm = props => {
       <PartnerSidenav />
 
       <h1 style={{ color: "green" }}>Create a Task</h1>
-      <input
+      <input class="form-control"
         style={{ margin: "50px auto", display: "block" }}
         type="text"
         name="name"
         placeholder="name"
       />
-      <input
+      <input class="form-control"
         style={{ margin: "50px auto", display: "block" }}
         type="text"
         name="description"
         placeholder="description"
       />
-      <input
+      <input class="form-control"
         style={{ margin: "50px auto", display: "block" }}
         type="text"
         name="wantsConsultant"
         placeholder="wantsConsultant"
       />
-      <input
+      <input class="form-control"
         style={{ margin: "50px auto", display: "block" }}
         type="text"
         name="field"
         placeholder="field"
       />
-
+              <div class="input-group mb-3">
+                  <input
+                        class="form-control"
+                        placeholder=" skills"
+                        onChange={props.change}
+                        type="text"
+                      />
+                      <div class="input-group-append">
+                      <button class="btn btn-outline-info" onClick={(e)=>{props.getUser2(e);}}>Add another skill</button>
+                      </div>
+                      </div>
 
       <button className="btn btn-success btn-sm m-2">Submit</button>
       <div
